5d8bf41b1c9a3ea7772cdc05fb14a9d8e1ef8921
Batuhan Özhan <batuhanozhan00@gmail.com>
2023-08-29 13:15:11 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Batuhan Özhan on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
